8d987d033acc8d81bca28a03f2233ab03c4c1115
rafi_ahmed <smrafi120@gmail.com>
2024-03-30 23:28:42 +0600

    practice printf and scanf function

---
+++ print_something.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int n = 10;
+    printf("Hello World\n");
+    printf("%d%% \n",n); //output 10%
+    return 0;
+}
---
+++ scan_something.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    int y;
+    char c;
+
+
+    scanf("%d%c %d%c",&x,&c,&y,&c);
+
+    printf("%d%% %d%%",x,y);
+    return 0;
+}
