714493550ef36e1b8f7805fc2bd8604274b4c070
rafisgithub <rafi.cse.ahmed@gmail.com>
2025-04-06 21:05:30 +0600

    introduction to programming module 1

---
+++ test/hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    
+    // char ch; 1 byte = 8 bit
+   char ch = "rafiahmed"; 
+
+   int size_ch = sizeof(ch);
+   
+   printf("%d",size_ch);
+
+
+
+    return 0;
+}
