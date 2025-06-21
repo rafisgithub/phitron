a4c5ee5c7e3752e9458e4926945982bbdf60b488
rafisgithub <rafi.cse.ahmed@gmail.com>
2025-06-21 07:29:29 +0600

    practice

--- C++ for Data Structure/stl-vector/C_Replacement.cpp
+++ C++ for Data Structure/stl-vector/C_Replacement.cpp
@@ -6,24 +6,22 @@ int main() {
 
     int n;
     cin >>n;
-    int arr[n];
+    vector<int> arr(n);
+
 
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
+
     for(int i = 0; i < n; i++) {
-           int temp = arr[0];
-
-    for(int i = 1; i < n; i++) {
-        arr[i-1] = arr[i];
+        if(arr[i] > 0){
+            arr[i] = 1;
+        }else if(arr[i]<0){
+            arr[i] = 2;
+        }
     }
 
-    arr[n-1] = temp;
-
-    }
-
-    
     for(int i = 0; i < n; i++) {
         cout << arr[i] <<" ";
     }
