3600d00d335ad2a0746a85624863b6d06c93b1e8
= <=>
2025-07-21 18:33:05 +0600

    Add solutions for "Remove Duplicate Again" and "A. Boy or Girl" problems
    - Implemented the "Remove Duplicate Again" problem in C++ and added test cases.
    - Created a test file for local testing of the solution.
    - Added necessary configuration files for Visual Studio Code to support C++ development.
    - Developed the "A. Boy or Girl" problem solution in both C++ and Python, including test cases.

---
+++ C++ for Data Structure/exam-on-linked-list/test.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int arr[8];
+
+    for(int i=0; i<8;i++) {
+        cin>>arr[i];
+    }
+
+    for(int i = 0;i<8;i++) {
+        for(int j=1;j<8;j++){
+            
+        }
+    }
+
+    for(int i=0; i<8;i++) {
+       cout << arr[i] <<" ";
+    }
+
+    return 0;
+}
